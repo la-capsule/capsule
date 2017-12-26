b24ad5428ade221b8bb63d26529abd0aa7b55179
Bastien <bastienrbt@protonmail.com>
2017-12-27 00:05:19 +0100

    Fix data-item-url for snipcart button

--- helpers/snipcart_helper.rb
+++ helpers/snipcart_helper.rb
@@ -1,5 +1,12 @@
 module SnipcartHelper
 
+  def snipcart_product_url(p)
+    return config[:host] +
+           (locale != I18n.default_locale ? '/' + lang.to_s : '') + '/' +
+           t("paths.catalog") + '/' +
+           p[locale].path
+  end
+
   def snipcart_button (p, text)
     args = {
       :"class" => "snipcart-add-item",
@@ -7,7 +14,7 @@ module SnipcartHelper
       :"data-item-price" => p.price.to_json,
       :"data-item-name" => p[locale].name,
       :"data-item-max-quantity" => p.max_quantity,
-      :"data-item-url" => p[locale].path,
+      :"data-item-url" => snipcart_product_url(p),
       :"data-item-image" => p.image
     }
 

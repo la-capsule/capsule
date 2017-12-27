b96c0d164af0c3351708cf36c4016a6c6a1fbaab
Bastien <bastienrbt@protonmail.com>
2017-12-27 15:17:41 +0100

    Remove useless : on snipcart button helper

--- helpers/snipcart_helper.rb
+++ helpers/snipcart_helper.rb
@@ -9,13 +9,13 @@ module SnipcartHelper
 
   def snipcart_button (p, text)
     args = {
-      :"class" => "snipcart-add-item",
-      :"data-item-id" => p.id,
-      :"data-item-price" => p.price.to_json,
-      :"data-item-name" => p[locale].name,
-      :"data-item-max-quantity" => p.max_quantity,
-      :"data-item-url" => snipcart_product_url(p),
-      :"data-item-image" => p.image
+      "class" => "snipcart-add-item",
+      "data-item-id" => p.id,
+      "data-item-price" => p.price.to_json,
+      "data-item-name" => p[locale].name,
+      "data-item-max-quantity" => p.max_quantity,
+      "data-item-url" => snipcart_product_url(p),
+      "data-item-image" => p.image
     }
 
     content_tag :button, args do

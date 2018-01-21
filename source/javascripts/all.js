f912087a6090ce975af4788246929db017e51dbf
Bastien <bastienrbt@protonmail.com>
2018-01-21 22:36:46 +0100

    Orders are now trackable from a page (using admin app API)

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,9 @@
 import Vue from 'vue/dist/vue.js'
 // import App from './vue/App.vue'
+import 'whatwg-fetch'
 import Radio from './components/radio.js'
 import Counter from './components/counter.js'
+import FollowOrder from './components/follow-order.js'
 
 // Set snipcart default currency
 Snipcart.api.cart.currency('usd');
---
+++ source/javascripts/components/follow-order.js
@@ -0,0 +1,21 @@
+class FollowOrder {
+  constructor () {
+    this.form = document.getElementById('follow-order')
+    this.order = this.form.querySelector("input[name='id']")
+    this.result = document.getElementById('follow-order-reponse')
+    this.form.addEventListener('submit', this.getOrder.bind(this))
+  }
+  getOrder () {
+    event.preventDefault()
+    fetch("https://capsule-admin.herokuapp.com/order/" + this.order.value + ".json").then((response) => {
+      response.json().then((json) => {
+        this.layout(json)
+      })
+    })
+  }
+  layout (json) {
+    this.result.innerHTML = JSON.stringify(json)
+  }
+}
+
+export default new FollowOrder

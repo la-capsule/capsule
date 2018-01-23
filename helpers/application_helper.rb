85a26406141b5f515962130d74c55355ef046245
Bastien <bastienrbt@protonmail.com>
2018-01-23 01:09:48 +0100

    Add nav link method to add active class on current_path == nav link path

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+  # Inspired by https://github.com/thoughtbot/middleman-aria_current
+  def nav_link(*arguments, aria_current: "page", **options, &block)
+    if block_given?
+      text = capture(&block)
+      path = arguments[0]
+    else
+      text = arguments[0]
+      path = arguments[1]
+    end
+
+    options = options.with_indifferent_access
+    current_path = current_page.url.to_s
+    current_path.gsub!('/', '') unless current_path == '/'
+
+    if current_path == path
+      if options[:class] != nil
+        options[:class] += " active"
+      else
+        options.merge!("class" => "active")
+      end
+    end
+
+    link_to(text, path, options)
+  end
 end

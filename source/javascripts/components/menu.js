4fbf253dafcddf90946570daad9fe4262b8802f0
Bastien <bastienrbt@protonmail.com>
2018-01-26 15:55:12 +0100

    Stylize menu for being responsive

--- source/javascripts/components/menu.js
+++ source/javascripts/components/menu.js
@@ -2,6 +2,7 @@ class Menu {
   constructor () {
     this.button = document.getElementById('menu')
     this.nav = document.querySelector('nav')
+    this.header = document.querySelector('header')
     this.footer = document.querySelector('footer')
     this.button.addEventListener('click', this.onClick.bind(this))
   }
@@ -12,12 +13,14 @@ class Menu {
     document.body.style.overflow = 'hidden'
     this.nav.classList.add('visible')
     this.footer.classList.add('visible')
+    this.header.classList.add('white')
     console.log('show')
   }
   hide () {
     document.body.style.overflow = 'inherit'
     this.nav.classList.remove('visible')
     this.footer.classList.remove('visible')
+    this.header.classList.remove('white')
     console.log('hide')
   }
 }
